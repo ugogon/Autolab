366a860fd27bc656b65b2ec9322871ecf030c967
xinyis991105 <xinyis@andrew.cmu.edu>
2020-10-22 14:41:52 +0800

    added placeholder for api endpoints

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -2,4 +2,12 @@ class MetricsController < ApplicationController
 	action_auth_level :index, :instructor
 	def index
 	end
+
+private
+	def get_current_metrics(course_id)
+	end
+
+	def update_current_metrics
+		# will use params containing information about the changed risk conditions
+	end
 end
