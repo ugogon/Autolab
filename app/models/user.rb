71bbbf1c8ea9484627693034345cc0ee1eb28158
Ugo Finnendahl <finnendahl@tu-berlin.de>
2021-10-25 16:08:13 +0200

    register diable seems problematic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, omniauth_providers: [:ldap]
 
